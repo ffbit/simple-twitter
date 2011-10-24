82ed540ac4d542cac774a557e96e633f44097729
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2011-10-24 21:32:52 +0300

    An email attribute and it's validation was described with RSpec for the User model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,39 @@ describe User do
     User.create!(@attr)
   end
   
+  describe "email attribute and it's validation" do
+    it "should have an email attribute" do
+      user = User.new(@attr)
+      user.should respond_to(:email)
+    end
+    
+    it "should require an email address" do
+      user = User.new(@attr.merge(:email => ""))
+      user.should_not be_valid
+    end
+    
+    it "should accept valid emails" do
+      emails = %w[good@yandex.ru p.h.d@example.i.com log12@in.five.org]
+      emails.each do |email|
+        valid_email_user = User.new(@attr.merge(:email => email))
+        valid_email_user.should be_valid
+      end
+    end
+    
+    it "should reject invalid emails" do
+      emails = %w[good@yandex,ru p.h.d_example.i.com log12g{at}in.five.org]
+      emails.each do |email|
+        invalid_email_user = User.new(@attr.merge(:email => email))
+        invalid_email_user.should_not be_valid
+      end
+    end
+    
+    it "should reject duplicated emails" do
+      User.create!(@attr)
+      user_with_duplicated_email = User.new(@attr)
+      user_with_duplicated_email.should_not be_valid
+    end
+  end
+  
 end
+
