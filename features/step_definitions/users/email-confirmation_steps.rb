898d86ed17408e9c17dfe3e1458f65f00dec989d
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2011-10-25 13:34:50 +0300

    A Email Confirmation after Sign up feature has been added with it's step definitions.

---
+++ features/step_definitions/users/email-confirmation_steps.rb
@@ -0,0 +1,12 @@
+Given /^I have (\d+) signed up unconfirmed user$/ do |quantity|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I confirm the user$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should have (\d+) confirmed user$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
