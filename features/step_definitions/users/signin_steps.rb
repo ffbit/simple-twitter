b70656c72451a4e3cd0ac334ed0e1dc3275a2246
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2011-10-25 17:22:32 +0300

    An Invalid Sing in with a wrong email/password combination scenario is green.

--- features/step_definitions/users/signin_steps.rb
+++ features/step_definitions/users/signin_steps.rb
@@ -12,3 +12,14 @@ When /^I sign in$/ do
   And %{I click a "Sign in" button}
 end
 
+Given /^I'm at the sign in page$/ do
+  visit path_to('the sign in page')
+end
+
+When /^I sing in with a wrong email\/password combination$/ do
+  When %{I fill in an "email" field with "not-signed-up@email.com"}
+  And %{I fill in a "password" field with ""}
+  And %{I click a "Sign in" button}
+end
+
+
--- features/users/signin.feature
+++ features/users/signin.feature
@@ -9,4 +9,11 @@ Feature: Sing in
     And I have a confirmed user
     When I sign in
     Then I should see "Signed in successfully."
+  
+  Scenario: Invalid Sing in with a wrong email/password combination
+    Given I'm not signed in
+    And I'm at the sign in page
+    When I sing in with a wrong email/password combination
+    Then I should see "Invalid email or password."
+    
 
