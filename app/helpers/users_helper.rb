85b5f0d84d9a7c424880c803e535a7d6f1c1d13f
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2011-10-27 22:31:48 +0300

    followers followeds.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  
+  def gravatar_for(user, options = { :size => 64})
+    gravatar_image_tag user.email, :alt => user.email,
+                                   :id => "avatar",
+                                   :class => 'avatar',
+                                   :gravatar => options
+  end
+  
 end
+
