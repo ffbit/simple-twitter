604400336d25010ba66d486aeedd095f4ba7172f
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2011-10-28 00:24:52 +0300

    A user_id parameter has been added to the more form.

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -2,10 +2,12 @@ module TweetsHelper
   
   def page_of_tweets(params)
     params[:page] = 0 if params[:page].nil?
+    params[:user_id] = current_user.id if params[:user_id].nil?
     @page = params[:page].to_i
+    user_id = params[:user_id].to_i
     limit = Tweet.per_page
     offset = @page * limit
-    @tweets = Tweet.find_all_by_user_id(current_user.id,
+    @tweets = Tweet.find_all_by_user_id(user_id,
                                         :offset => offset, :limit => limit)
     if (@tweets.count + offset) == current_user.tweets.count
       @page = -1
