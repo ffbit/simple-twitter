59ea8549b145cc227928ec9c4fc19cf239e254f9
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2011-10-28 01:06:44 +0300

    Pagination

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -2,14 +2,21 @@ module TweetsHelper
   
   def page_of_tweets(params)
     params[:page] = 0 if params[:page].nil?
-    params[:user_id] = current_user.id if params[:user_id].nil?
     @page = params[:page].to_i
-    user_id = params[:user_id].to_i
     limit = Tweet.per_page
     offset = @page * limit
-    @tweets = Tweet.find_all_by_user_id(user_id,
-                                        :offset => offset, :limit => limit)
-    if (@tweets.count + offset) == current_user.tweets.count
+    
+    # Brute force!!!
+    params[:user_id] = current_user.id if params[:user_id].nil?
+    user = User.find_by_id(params[:user_id])
+    ids = [current_user.id]
+    user.following.each do |followed|
+      ids << followed.id
+    end
+    
+    @tweets = Tweet.find_all_by_user_id(ids, :offset => offset, :limit => limit)
+    
+    if @tweets.last == Tweet.find_last_by_user_id(ids)
       @page = -1
     end
       @page = @page + 1
