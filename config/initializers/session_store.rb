be8ef96a51214adc639e647907b7ae7b1759a1a2
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2011-11-01 20:30:11 +0200

    Rails were downgraded.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-SimpleTwitter::Application.config.session_store :cookie_store, key: '_simple-twitter_session'
+SimpleTwitter::Application.config.session_store :cookie_store,
+                                                :key => '_simple-twitter_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
