47880298c114589e2e6bab77cb2618f8dc83489d
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2011-11-02 00:05:29 +0200

    The Relationship controller has been described.

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe RelationshipsController do
-
+  include Devise::TestHelpers
+  render_views
+  
+  let(:relationship) { Factory :relationship }
+  
+  describe "PUT 'update'" do
+    it "for non-signed-in users" do
+      put :update, :id => relationship.followed_id
+      response.should redirect_to(new_user_session_path)
+    end
+  end
+  
+  describe "DELETE 'destroy'" do
+    it "for non-signed-in users" do
+      delete :destroy, :id => relationship.followed_id
+      response.should redirect_to(new_user_session_path)
+    end
+  end
 end
